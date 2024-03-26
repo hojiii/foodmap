8279ffc37408e5cfe70946ac094f8728fef97d8e
hojiii <shimn3060@naver.com>
2024-03-27 00:05:53 +0900

    className에 tailwind 직접 적용

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "80d17618c85f15d8"; }
+/******/ 		__webpack_require__.h = function() { return "01cee4f396a12cf9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
